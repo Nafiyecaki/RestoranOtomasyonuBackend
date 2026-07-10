d0338d13e59f34f20ffda9966a8ef082110092d8
Berat Safa Kahraman <163014361+beratsafaa@users.noreply.github.com>
2026-07-10 09:14:29 +0300

    güncellendi

--- src/Restoran.API/Program.cs
+++ src/Restoran.API/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi(); // .NET Yerleþik OpenAPI geldi mi
 
 builder.Services.AddDbContext<DbRestoranContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionNNNNnnnnnnn")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Debörböðbwlö")));
 
 var app = builder.Build();
 
