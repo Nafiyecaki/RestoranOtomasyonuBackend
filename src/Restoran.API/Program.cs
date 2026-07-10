34d45da2eaa29bc31b5fef3b1859e507ad90a867
ilayydadoggan <ilaydadogan077@gmail.com>
2026-07-10 09:21:16 +0300

    deneme tekrar yapıldı.

--- src/Restoran.API/Program.cs
+++ src/Restoran.API/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi(); // .NET Yerleþik OpenAPI geldi mi
 
 builder.Services.AddDbContext<DbRestoranContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString(" zzzzz")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("deneme123")));
 
 var app = builder.Build();
 
