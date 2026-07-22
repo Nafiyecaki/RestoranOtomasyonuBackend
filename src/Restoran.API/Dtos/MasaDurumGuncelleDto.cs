006bb5118703983663b874ad5302b08d863024a0
cakinafiye <cakinafiye1@gmail.com>
2026-07-22 11:23:01 +0300

    Garson sayfasında iyileştirmeler yapıldı.

---
+++ src/Restoran.API/Dtos/MasaDurumGuncelleDto.cs
@@ -0,0 +1,8 @@
+﻿// Dtos/MasaDurumGuncelleDto.cs
+namespace Restoran.API.Dtos;
+
+public class MasaDurumGuncelleDto
+{
+    public string MasaDurumu { get; set; } = null!;
+    public string? RezervasyonSaati { get; set; } // "19:00" gibi, opsiyonel
+}
